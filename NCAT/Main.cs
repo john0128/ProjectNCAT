397f39c923e98abaf67cb14d773056d89be70b0d
NineJuan <juanjr123@naver.com>
2022-01-27 23:15:18 +0900

    알맞지 않은 명령어 입력 시 오류문구 출력

--- NCAT/Main.cs
+++ NCAT/Main.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace NCAT
 {
@@ -14,14 +16,34 @@ namespace NCAT
             Console.Write("|");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Developed By NineJuan N913");
-            Console.ForegroundColor= ConsoleColor.Green;
+            Console.ForegroundColor= ConsoleColor.Green; 
+            Console.Write("|"); //제작자 표시
+            Console.WriteLine("");
+            Console.WriteLine("");//한칸 띄기
             Command();
         }
         public void Command()
         {
+            Console.ForegroundColor = ConsoleColor.White;
+            string localIP = string.Empty;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530); //
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            Console.Write($"{Environment.MachineName}@{localIP}>"); //PC이름@자신의 IP주소> 로 출력된다
+            var Input = Console.ReadLine();
             for (; ; )
             {
-
+                if (Input == "help")
+                {
+                    Console.WriteLine("help");
+                    Command();
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Unavailable Command!\nPlease Use Another Command");
+                Command(); //만약 지정된 명령어가 아닐 경우 빨간 글씨로 위 문구 출력
             }
         }
     }
